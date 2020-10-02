1ea4b9cbc61e3fdf32017ac80d3136d3265a2507
General Assembly <generalassembly@Davids-MacBook-Pro.local>
2020-10-02 16:44:05 -0700

    [date_crud] Stub out GET routes for date_crud

--- crud/date_crud.py
+++ crud/date_crud.py
@@ -1,3 +1,20 @@
 from flask import jsonify, redirect
 from models import db, Date
 
+# GET all
+def get_all_dates():
+    all_dates = Date.query.all()
+    results = [date.as_dict() for date in all_dates]
+    return jsonify(results)
+
+# GET one
+def get_date(id):
+    date = Date.query.get(id)
+    if date:
+        return jsonify(date.as_dict())
+    else:
+        raise Exception('Sorry, could not find date at {}🤷‍♂️'.format(id))
+
+# POST
+
+# DELETE
