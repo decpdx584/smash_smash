54edb11c38f3469826bc196b9c06754919f4185d
General Assembly <generalassembly@Davids-MacBook-Pro.local>
2020-10-02 09:29:25 -0700

    [models] Fix models, FINALLY successfully create tables in database

--- models.py
+++ models.py
@@ -7,10 +7,10 @@ app.config['SQLALCHEMY_ECHO'] = True
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://localhost/smash_dev'
 db = SQLAlchemy(app)
 
-date_args = db.Table('date_args',
-    db.Column('date_id', db.Integer, db.ForeignKey('date_id'), primary_key=True),
-    db.Column('argument_id', db.Integer, db.ForeignKey('argument_id'), primary_key=True)
-)
+# fighter_dates = db.Table('fighter_dates',
+#     db.Column('fighter_id', db.Integer, db.ForeignKey('fighters.id'), primary_key=True)
+#     db.Column('date_id', db.Integer, db.ForeignKey('dates.id'), primary_key=True),
+# )
 
 class Fighter(db.Model):
     __tablename__ = 'fighters'
@@ -22,6 +22,7 @@ class Fighter(db.Model):
     final_smash = db.Column(db.String)
     quote = db.Column(db.String)
     wiki_url = db.Column(db.String)
+
     dates = db.relationship('Date', backref='fighter', lazy=True)
 
     def __repr__(self):
@@ -30,16 +31,18 @@ class Fighter(db.Model):
     def as_dict(self):
         return {c.name: getattr(self, c.name) for c in self.__table__.columns}
 
+date_args = db.Table('date_args',
+    db.Column('date_id', db.Integer, db.ForeignKey('dates.id'), primary_key=True),
+    db.Column('argument_id', db.Integer, db.ForeignKey('arguments.id'), primary_key=True)
+)
+
 class Date(db.Model):
     __tablename__ = 'dates'
 
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String, nullable=False)
-    email = db.Column(db.String, unique=True, nullable=False)
-    bio = db.Column(db.String(150))
+    fighter_one = db.Column(db.Integer, db.ForeignKey('fighters.id', ondelete='SET NULL'), nullable=False)
+    fighter_two = db.Column(db.Integer, db.ForeignKey('fighters.id', ondelete='SET NULL'), nullable=False)
 
-    fighter_one = db.relationship('Fighter', backref='single', lazy=True)
-    fighter_two = db.relationship('Fighter', backref='single', lazy=True)
     arguments = db.relationship('Argument', backref='date', lazy=True)
 
     def __repr__(self):
@@ -54,4 +57,10 @@ class Argument(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     author = db.Column(db.String(50))
     body = db.Column(db.String, nullable=False)
-    date_id = db.Column(db.Integer, db.ForeignKey('date_id'), nullable=False)
+    date_id = db.Column(db.Integer, db.ForeignKey('dates.id', ondelete='SET NULL'), nullable=False)
+
+    def __repr__(self):
+        return f'Argument(id={self.id}, author="{self.author}", body="{self.body}")'
+
+    def as_dict(self):
+        return {c.name: getattr(self, c.name) for c in self.__table__.columns}
