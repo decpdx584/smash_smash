4ab543e194faac8b2afd2fa47e58ec33eeff16fe
General Assembly <generalassembly@Davids-MacBook-Pro.local>
2020-10-04 18:37:14 -0700

    [date_crud] Successdully add dates index page, stub out date_show page and route which still need some love

--- api.py
+++ api.py
@@ -2,6 +2,7 @@ from models import db, app, Fighter_one, Fighter_two, Date, Argument
 from flask import Flask, jsonify, request, g, redirect, url_for, render_template
 from flask_sqlalchemy import SQLAlchemy
 from crud.fighter_crud import get_all_fighters, get_fighter, create_fighter, update_fighter, destroy_fighter
+from crud.date_crud import get_all_dates, get_date
 
 # ERROR HANDLER
 @app.errorhandler(Exception)
@@ -17,7 +18,8 @@ def unhandled_exception(e):
 def home():
     return render_template('home.html')
 
-## GET index, POST new
+## Fighters:
+### GET index, POST new
 @app.route('/singles', methods=['GET', 'POST'])
 def fighter_index_create():
     if request.method == 'GET':
@@ -28,9 +30,28 @@ def fighter_index_create():
 ## GET show, PUT update, DELETE destroy
 @app.route('/singles/<int:id>')
 def fighter_show_put_delete(id):
-    return render_template('singles/single_show.html', single = get_fighter(id))
-    # if request.method == 'GET':
-    # if request.method == 'PUT':
-    #     return update_user(id, **request.form)
-    # if request.method == 'DELETE':
-    #     return destroy_user(id)
+    if request.method == 'GET':
+        return render_template('singles/single_show.html', single = get_fighter(id))
+    if request.method == 'PUT':
+        return update_user(id, **request.form)
+    if request.method == 'DELETE':
+        return destroy_user(id)
+
+## Dates:
+### GET index, POST new
+@app.route('/dates', methods=['GET', 'POST'])
+def date_index_create():
+    if request.method == 'GET':
+        return render_template('dates/dates.html', dates = get_all_dates())
+    if request.method == 'POST':
+        return create_fighter(**request.form)
+
+## GET show, PUT update, DELETE destroy
+@app.route('/dates/<int:id>')
+def date_show_put_delete(id):
+    if request.method == 'GET':
+        return render_template('dates/date_show.html', date = get_date(id))
+    if request.method == 'PUT':
+        return update_user(id, **request.form)
+    if request.method == 'DELETE':
+        return destroy_user(id)
--- crud/date_crud.py
+++ crud/date_crud.py
@@ -4,14 +4,14 @@ from models import db, Date
 # GET all
 def get_all_dates():
     all_dates = Date.query.all()
-    results = [date.as_dict() for date in all_dates]
-    return jsonify(results)
+    # results = [date.as_dict() for date in all_dates]
+    return all_dates
 
 # GET one
 def get_date(id):
     date = Date.query.get(id)
     if date:
-        return jsonify(date.as_dict())
+        return date
     else:
         raise Exception('Sorry, could not find date at {}🤷‍♂️'.format(id))
 
